907bd75cbb5b1908da3f36907caf1f99bc0209cf
Eugene-prog <ieugene.prog@gmail.com>
2023-07-20 22:10:40 +0700

    сделано решение пятого урока

---
+++ lesson-01/05.js
@@ -0,0 +1,13 @@
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом";
+
+switch (true) {
+  case (rule.length === 25):
+     console.log('50 на 50');
+    break;
+  case (rule.length < 25):
+    console.log('Все таки нет правил без исключения');
+    break;
+  default:
+    console.log('У меня 100 пудов все получится');
+    break;
+}
