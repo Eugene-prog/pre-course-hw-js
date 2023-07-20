3ca25d1bc9fc76313afe34631b79cc3f5d4cabc4
Eugene-prog <ieugene.prog@gmail.com>
2023-07-20 21:20:30 +0700

    сделано решение третьего урока v2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,16 +6,3 @@ if (number1 % 2 === 0) {
 } else {
    console.log(`[${number1}]:Это нечетное число`);  
 }  
-
-let number = 1;
-
-console.log(number); // 1
-++number;
-number++;
-console.log(number); // 3
-
-console.log(number); // 3
-
-number--;
---number;
-console.log(number); // 1
---
+++ lesson-01/03.js
@@ -0,0 +1,12 @@
+let number = 1;
+
+console.log(number); // 1
+++number;
+number++;
+console.log(number); // 3
+
+console.log(number); // 3
+
+number--;
+--number;
+console.log(number); // 1
