00b8d3493f40b76eb724ecab5a064e8706137488
Eugene-prog <ieugene.prog@gmail.com>
2023-07-20 23:22:52 +0700

    сделано решение шестого урока

---
+++ lesson-01/06.js
@@ -0,0 +1,3 @@
+//let str = "zzzzyzz"; используя тернарный оператор и метод includes. Если нашел букву "y", то отконсоль "нашел", если не нашел, то"заблудился"
+let str = "zzzzyzz";
+str.includes("y") ? console.log("нашел") : console.log("заблудился");
