32427019d6d0179bece989df4b4e69694c913dc0
Eugene-prog <ieugene.prog@gmail.com>
2023-07-20 21:04:21 +0700

    сделано решение второго урока

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,8 @@
 // code
+let number1 = Math.floor(Math.random() * 100);
+
+if (number1 % 2 === 0) {
+  console.log(`[${number1}]:Это четное число`);
+} else {
+   console.log(`[${number1}]:Это нечетное число`);  
+}  
