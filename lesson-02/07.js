a77732f45cc09dff052bcc35c88b0baf0c5da65b
Eugene-prog <ieugene.prog@gmail.com>
2023-07-22 23:47:21 +0700

    решение 7 задания 2 неделя v2

---
+++ lesson-02/07.js
@@ -0,0 +1,13 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+console.log(passportMarried);
+
+passportMarried.married = true;
+
+console.log(passportMarried);
