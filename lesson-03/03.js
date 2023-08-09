1895c8ca154ac06e27a55b0126edb8494f95db8f
Eugene-prog <ieugene.prog@gmail.com>
2023-08-09 21:28:43 +0700

    добавлено решение задача 3 урок 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -4,6 +4,6 @@
 
 ​const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-const slicedFruits = fruits.slice(1, 9);
+const slicedFruits = fruits.slice(1, 8);
 
 console.log(slicedFruits);
